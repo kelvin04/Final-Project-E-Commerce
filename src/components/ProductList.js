3442c153c941a2fbfd0ffe257b87fb084ea95a6f
kelvin <kelvin.tertia@gmail.com>
2018-09-26 18:15:14 +0700

    wednesday commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -29,7 +29,7 @@ class ProductList extends Component {
           <Col xs={12} md={4} lg={3}>
               <Thumbnail src={image1} alt="242x200" style={{ textAlign:"center" }}>
                 <h4 style={{ fontWeight:"bold" }}>{item.ProductName}</h4>
-                <h6 style={{ color:"red", fontWeight:"bold"}}>NEW Product!</h6>
+                <h5 style={{ color:"blue", fontWeight:"bold"}}>NEW Product!</h5>
                 <h4 style={{ color:"#ff5722", fontWeight:"bold" }}>Rp. {(parseInt(item.SalePrice)).toLocaleString('id')},-</h4>
                 <h4 style={{ textAlign:"center" }}>
                   <Button bsStyle="success">Add To Cart</Button>
@@ -40,11 +40,28 @@ class ProductList extends Component {
               </Col>
         );
       }
+      else if(item.NormalPrice == 1) {
+        return (
+          <Col xs={12} md={4} lg={3}>
+              <Thumbnail src={image1} alt="242x200" style={{ textAlign:"center" }}>
+                <h4 style={{ fontWeight:"bold" }}>{item.ProductName}</h4>
+                <h5 style={{ color:"red", fontWeight:"bold"}}>HOT ITEM !!!</h5>
+                <h4 style={{ color:"#ff5722", fontWeight:"bold" }}>Rp. {(parseInt(item.SalePrice)).toLocaleString('id')},-</h4>
+                <h4 style={{ textAlign:"center" }}>
+                  <Button bsStyle="success">Add To Cart</Button>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <Link to="/productdetails"><Button bsStyle="default">Details</Button></Link>
+                </h4>
+              </Thumbnail>
+              </Col>
+        );
+      }
+
       return(
         <Col xs={12} md={4} lg={3}>
               <Thumbnail src={image1} alt="242x200" style={{ textAlign:"center" }}>
                 <h4 style={{ fontWeight:"bold" }}>{item.ProductName}</h4>
-                <h6 style={{ color:"#aaaaaa", fontWeight:"bold", textDecoration:"line-through" }}>Rp. {(parseInt(item.NormalPrice)).toLocaleString('id')},-</h6>
+                <h5 style={{ color:"#aaaaaa", fontWeight:"bold", textDecoration:"line-through" }}>Rp. {(parseInt(item.NormalPrice)).toLocaleString('id')},-</h5>
                 <h4 style={{ color:"#ff5722", fontWeight:"bold" }}>Rp. {(parseInt(item.SalePrice)).toLocaleString('id')},-</h4>
                 <h4 style={{ textAlign:"center" }}>
                   <Button bsStyle="success">Add To Cart</Button>
--- src/components/SideTab.js
+++ src/components/SideTab.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 import { Tab, NavItem, Row, Col, Nav } from 'react-bootstrap';
-import {CircleArrow as ScrollUpButton} from "react-scroll-up-button";
+import { CircleArrow as ScrollUpButton } from "react-scroll-up-button";
+import Select from 'react-select';
 import ProductList from './ProductList';
 import iconGaming from '../images/iphone_icon.png';
 
 class SideTab extends Component {
     render() { 
+        const scaryAnimals = [
+            { label: "Apple", value: 1 },
+            { label: "OPPO", value: 2 },
+            { label: "Samsung", value: 3 },
+            { label: "Xiaomi", value: 4 },
+        ];
         return ( 
             <div>
             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
                 <Row className="clearfix">
-                    <Col sm={2}  id="side-tab">
+                    <Col sm={2} id="side-tab">
                     <Nav bsStyle="pills" stacked>
                         <NavItem eventKey="first"><span ><img id="icon-scale" src={iconGaming}></img></span>Smartphone</NavItem>
                         <NavItem eventKey="second">Tab 2</NavItem>
                     </Nav>
                     </Col>
-                    {/* <Col sm={10} style={{ paddingLeft: "6px"}}> */}
                     <Col sm={10} id="product-list">
                     <Tab.Content animation>
                         <Tab.Pane eventKey="first" >
-                        
+                        {/* <div style={{ display:"flex", d }}>
+                            
+                        </div> */}
+
+                        <div style={{ margin:"0px 10px 25px 15px" }}>
+                            Filter by Brand :
+                            <Select options={scaryAnimals} isMulti/>
+                        </div>
                             <ProductList />
                         </Tab.Pane>
                         <Tab.Pane eventKey="second">Tab 2 content</Tab.Pane>
